70c9dec43a628005f4930799b18987f0225c526f
Juan <juan_jg@hotmail.com>
2021-01-03 17:49:52 -0300

    adicao exercicio 5 condicionais

--- python-play-mugol/condicionales/play_ejr05_Guanabara.py
+++ python-play-mugol/condicionales/play_ejr05_Guanabara.py
@@ -20,3 +20,58 @@
 # preço  total,  tipo de  pagamento,  valor do desconto e 
 # valor a pagar.
 
+print ("===================================")
+print("\tHipermercado Guanabara")
+print ("===================================")
+print("Promocao de carne: ")
+print("a. File Bobino")
+print("b. Alcatra")
+print("c. Picanha")
+
+tipoCarne = input("Tipo de carne: ")
+cantidad = float(input("cantidad (Kg): "))
+cartao = input("Cartao Tabajara: ")
+tipo = ''
+preco = 0.0
+precoTotal = 0.0
+tipoPagamento = '' 
+desconto = 0.0
+
+if (tipoCarne == 'a'):
+    tipo = "File Bobino"
+    if (cantidad <= 5):
+        preco = cantidad * 10.90
+    else: 
+        preco = cantidad * 9.80
+
+if (tipoCarne == 'b'):
+    tipo = "Alcatra"
+    if (cantidad <= 5):
+        preco = cantidad * 15.90
+    else: 
+        preco = cantidad * 13.80
+
+if (tipoCarne == 'c'):
+    tipo = "Picanha"
+    if (cantidad <= 5):
+        preco = cantidad * 20.90
+    else: 
+        preco = cantidad * 17.80
+
+if (cartao == 'sim' or cartao == 's'):
+    desconto = preco * 0.05
+    precoTotal = preco - desconto
+    tipoPagamento = "cartao Trabajara"
+else: 
+    desconto = 0.0
+    precoTotal = preco
+    tipoPagamento = "dinheiro"
+
+print ("===================================")
+print("Tipo de carne : %s" % (tipo))
+print("Cantidad (Kg) : %0.2f" % (cantidad))
+print("Preco Total   : %0.2f" % (preco))
+print("Tipo de pagamento: %s" % (tipoPagamento))
+print("Valor de desconto: %0.2f" % (desconto))
+print("Valor a pagar: %0.2f" % (precoTotal))
+print ("===================================")
